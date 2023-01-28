87e4b07839cd002496a81414ba773643d5d497bd
jumartinor <102238463+jumartinor@users.noreply.github.com>
2023-01-28 19:25:02 +0100

    bow

---
+++ Assets/_JULIA/Scripts/BowStrip.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class BowStrip : MonoBehaviour
+{
+    
+    [SerializeField] private Transform endpoint_1, endpoint_2;
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    public void CreateString(Vector3? midPosition) //el ? es porque el vector puede ser null
+    {
+        Vector3[] linePoints = new Vector3[midPosition == null ? 2 : 3];
+        linePoints[0] = endpoint_1.localPosition;
+        if (midPosition != null)
+        {
+            linePoints[1] = transform.InverseTransformPoint(midPosition.Value);
+        }
+        linePoints[^1] = endpoint_2.localPosition;
+
+        lineRenderer.positionCount = linePoints.Length;
+        lineRenderer.SetPositions(linePoints);
+    }
+
+    private void Start()
+    {
+        CreateString(null);
+    }
+}
