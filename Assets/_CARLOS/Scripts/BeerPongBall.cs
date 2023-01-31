0684777c5e69eb63a336d5f6ecabaa45f7c0156b
Carlos Cuevas <ccuevasbalinas@gmail.com>
2023-02-01 00:07:36 +0100

    creo que funciona el beer pong

--- Assets/_CARLOS/Scripts/BeerPongBall.cs
+++ Assets/_CARLOS/Scripts/BeerPongBall.cs
@@ -5,25 +5,33 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BeerPongBall : MonoBehaviour
 {
-    public bool isPlayerBall;
-
-    private Transform _transform;
-
-    private Rigidbody _rigidbody;
-
+    [SerializeField] private bool _isPlayerBall = false;
+    [SerializeField] private float _launchSpeed = 10.0f;
     [SerializeField] private Transform _ballSpawn;
-
     [SerializeField] private LayerMask _targetLayer;
     [SerializeField] private LayerMask _floorLayer;
 
-    [SerializeField] private ScriptableEvent _OnEnterCupEvent;
-    
+    [SerializeField] private ScriptableEvent _endOfTurnEvent;
+    [SerializeField] private ScriptableEvent _enterPlayerCupEvent;
+    [SerializeField] private ScriptableEvent _enterRivalCupEvent;
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;
 
     private void Awake()
     {
         _transform = GetComponent<Transform>();
-        _rigidbody = GetComponent<Rigidbody>(); 
-        gameObject.SetActive(false);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        Invoke("EndOfTurn", 30); 
+    }
+
+    public void EndOfTurn()
+    {
+        _endOfTurnEvent.Raise();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,16 +39,25 @@ public class BeerPongBall : MonoBehaviour
         // Ball triggers cup
         if (_targetLayer == (1 << other.gameObject.layer | _targetLayer))
         {
-            Debug.Log("PELOTA ENTRA EN CUP");
+            if(_isPlayerBall)
+            {
+                Debug.Log("PELOTA ENTRA EN CUP DEL RIVAL");
+                _enterRivalCupEvent.Raise();
+            }
+            else
+            {
+                Debug.Log("PELOTA ENTRA EN CUP DEL JUGADOR");
+                _enterPlayerCupEvent.Raise();
+            }
             other.gameObject.transform.parent.gameObject.SetActive(false);
-            ResetBall();
-            _OnEnterCupEvent.Raise();
+            EndOfTurn();
+           
         }
         // Ball triggers floor
         if (_floorLayer == (1 << other.gameObject.layer | _floorLayer))
         {
             Debug.Log("PELOTA TOCA EL SUELO");
-            ResetBall();
+            EndOfTurn();
         }
     }
 
@@ -49,6 +66,19 @@ public class BeerPongBall : MonoBehaviour
         _rigidbody.velocity = Vector3.zero;
         _rigidbody.angularVelocity = Vector3.zero;
         _transform.position = _ballSpawn.position;
+    }
+
+    public void ParabolicLaunch()
+    {
+        var angle = Random.Range(30.0f, 60.0f) * Mathf.Deg2Rad;
+
+        var direction = _transform.forward;
+        direction.y = Mathf.Tan(angle);
+        direction = direction.normalized;
+
+        //var direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0.0f);
+
+        _rigidbody.velocity = direction * _launchSpeed;
     }
 
 }
--- Assets/_CARLOS/Scripts/BeerPongGameManager.cs
+++ Assets/_CARLOS/Scripts/BeerPongGameManager.cs
@@ -6,11 +6,108 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BeerPongGameManager : MonoBehaviour
 {
+    private int _placedCups = 0;
+    private int _beerPongCups;
+    private int _remainPlayerCups;
+    private int _remainRivalCups;
+
+    private bool _turnOfPlayer = true;
+    private bool _endOfGame = false;
+
+    [SerializeField] private GameObject _playerBall;
+    [SerializeField] private GameObject _rivalBall;
+
+    [SerializeField] private ScriptableEvent StartBeerPongGameEvent;
+    [SerializeField] private ScriptableEvent EndBeerPongGameEvent;
+
+    private void Awake()
+    {
+        _beerPongCups = _remainPlayerCups = _remainRivalCups = 6;
+    }
+
+    // Game Preparation
+    public void PlaceCup()
+    {
+        _placedCups = _placedCups + 1;
+        if(_placedCups == _beerPongCups) 
+        {
+            Debug.Log("Todas los vasos colocados");
+            StartBeerPongGameEvent.Raise();
+        }
+    }
+
+    public void TakeOffPlayerCup()
+    {
+        _remainPlayerCups = _remainPlayerCups - 1;
+        if (_remainPlayerCups == 0)
+        {
+            Debug.Log("Pierde el jugador");
+            _endOfGame = true;
+        }
+    }
+
+    public void TakeOffRivalCup()
+    {
+        _remainRivalCups = _remainRivalCups - 1;
+        if (_remainRivalCups == 0)
+        {
+            Debug.Log("Gana el jugador");
+            _endOfGame = true;
+        }
+    }
+
+    public void HandleTurn()
+    {
+        if(!_endOfGame)
+        {
+            if (_turnOfPlayer)
+            {
+                Debug.Log("Turno del jugador");
+                TurnOfPlayer();
+            }
+            else
+            {
+                Debug.Log("Turno del rival");
+                TurnOfRival();
+            }
+        }
+        else
+        {
+            EndBeerPongGameEvent.Raise();
+        }
+
+    }
+
+    public void SwitchTurn()
+    {
+        _turnOfPlayer = !_turnOfPlayer;
+    }
+
+    public void TurnOfPlayer()
+    {
+        _playerBall.SetActive(true);
+        _playerBall.GetComponent<BeerPongBall>().ResetBall();
+    }
+
+    public void TurnOfRival()
+    {
+        _rivalBall.SetActive(true);
+        var rivalBall = _rivalBall.GetComponent<BeerPongBall>();
+        rivalBall.ResetBall();
+        StartCoroutine(WaitTimeCoroutine(1.5f));
+        rivalBall.ParabolicLaunch();
+    }
+
+    private IEnumerator WaitTimeCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+    /*
     [SerializeField] private GameObject _playerBall;
     [SerializeField] private List<GameObject> _playerCups;
     [SerializeField] private List<GameObject> _cupsSockets;
 
-    private int _placedCups;
     private int _cupsToPlace;
 
     private int _remainPlayerCups;
@@ -57,27 +154,11 @@ public class BeerPongGameManager : MonoBehaviour
         return _remainRivalCups == 0;
     }
 
-    public void TakeOffPlayerCup()
-    {
-        _remainPlayerCups = _remainPlayerCups - 1;
-        if(AllPlayerCupsOut())
-        {
-            Debug.Log("Gana Rival");
-        }
-    }
 
-    public void TakeOffRivalCup()
-    {
-        _remainRivalCups = _remainRivalCups - 1;
-        if(AllRivalCupsOut()) 
-        {
-            Debug.Log("Gana Jugador");
-        }
-    }
 
     public void Test()
     {
         Debug.Log("Hola");
     }
-
+    */
 }
