7336d7fc859a1fe35999cb2e6886a7e6c78836bf
jumartinor <102238463+jumartinor@users.noreply.github.com>
2023-02-02 14:17:02 +0100

    bowZ

--- Assets/_JULIA/Scripts/BowStringController.cs
+++ Assets/_JULIA/Scripts/BowStringController.cs
@@ -8,8 +8,9 @@ public class BowStringController : MonoBehaviour
 {
     [SerializeField] private BowString bowStringRenderer; //cuerda del arco 
     private XRGrabInteractable interactable;
-    [SerializeField] Transform midPointGrabObject;
+    [SerializeField] Transform midPointGrabObject, midPointVisualObject, midPointParent;
     private Transform interactor;
+    [SerializeField] private float bowStringStrechLimit = 0.3f;
 
     private void Awake(){
         interactable = midPointGrabObject.GetComponent<XRGrabInteractable>();
@@ -25,6 +26,7 @@ public class BowStringController : MonoBehaviour
         //vuelve al centro cuando se suelta
         interactor = null;
         midPointGrabObject.localPosition = Vector3.zero;//posicion local cero corresponde a la posicion inicial en el centro de la cuerda
+        midPointVisualObject.localPosition = Vector3.zero;
         bowStringRenderer.CreateString(null);//recrear la cuerda recta
     }
 
@@ -37,6 +39,12 @@ public class BowStringController : MonoBehaviour
     private void Update()
     {
         if(interactor != null){
+
+            //convierte la posicion del punto medio de la cuerda a posicion local del midPointGrabObject
+            //la posicion de la mano se convierte en la posicion local del midPointGrabObject
+            Vector3 midPointLocalSpace = midPointParent.InverseTransformPoint(midPointGrabObject.position);
+            //offset en valor absoluto de separación de la cuerda en el eje z
+            float midPointLocalAbsolute = Math.Abs(midPointLocalSpace.z);
             //crea la cuerda de acuerdo al movimiento del midPointGrabObject
             bowStringRenderer.CreateString(midPointGrabObject.transform.position);
         }
