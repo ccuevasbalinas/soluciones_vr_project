d1c73d7d2b1a48c743e366bfe2a5ffb9867a0114
jumartinor <102238463+jumartinor@users.noreply.github.com>
2023-02-18 20:15:27 +0100

    arrow movement fixed

--- Assets/_JULIA/Scripts/Archery/ArrowMovement.cs
+++ Assets/_JULIA/Scripts/Archery/ArrowMovement.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class ArrowMovement : MonoBehaviour
 {
     [SerializeField]private GameObject arrow;
-    private float speed = 3f;
+    private float speed = 0.01f;
+    [SerializeField] Transform attachPoint;
    
     // Update is called once per frame
     void Update()
     {
         if (arrow.activeInHierarchy)
         {
-            arrow.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            arrow.transform.position = attachPoint.position + new Vector3(speed * Time.deltaTime, 0, 0);
         }
     }
 }
